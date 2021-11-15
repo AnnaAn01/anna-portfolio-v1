41bee133ee1d5d981175533c085a01e84fc40508
Anna An <arzumanyan.an@gmail.com>
2021-11-15 10:10:58 +0400

    Added side menu item

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,15 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Photo from "../img/photoSketch.jpg";
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData.js";
+import * as MdIcons from "react-icons/md";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <nav>
-        <img src={Photo} alt="" className="photo-sketch" />
+      <nav className="side-menu">
         <ul className="side-menu-items">
+          <Link to="/" className="photo-sketch">
+            <img src={Photo} alt="Profile" className="photo-sketch" />
+          </Link>
+          <Link to="/" className="name-link photo-sketch">
+            <div className="my-name">ANNA DEVELOPER</div>
+            <div className="front-end">FRONT-END DEVELOPER</div>
+          </Link>
           {SidebarData.map((item, index) => {
             return (
               <li key={index} className={item.cName}>
@@ -21,6 +28,42 @@ function Sidebar() {
               </li>
             );
           })}
+          <div className="get-in-touch">GET IN TOUCH</div>
+          <div className="sidebar-social-media-icons">
+            <span>
+              <a target="_blank" rel="noreferrer" href="https://gmail.com">
+                <MdIcons.MdEmail className="sidebar-sns" />
+              </a>
+            </span>
+            <span>
+              <a target="_blank" rel="noreferrer" href="https://linkedin.com">
+                <FaIcons.FaLinkedin className="sidebar-sns" />
+              </a>
+            </span>
+            <span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/AnnaAn01"
+              >
+                <FaIcons.FaGithubSquare className="sidebar-sns" />
+              </a>
+            </span>
+            <span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/AnnaSoftwareDev"
+              >
+                <FaIcons.FaTwitterSquare className="sidebar-sns" />
+              </a>
+            </span>
+            <span>
+              <a target="_blank" rel="noreferrer" href="https://youtube.com">
+                <FaIcons.FaYoutube className="sidebar-sns" />
+              </a>
+            </span>
+          </div>
         </ul>
       </nav>
     </div>
