2e6b8d4c4ac46900fd5d16fff7b36278372dbda1
Anna An <arzumanyan.an@gmail.com>
2021-11-15 08:55:33 +0400

    Added menu items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Photo from "../img/photoSketch.jpg";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "./SidebarData.js";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <img src={Photo} alt="" className="photo-sketch" />
+      <nav>
+        <img src={Photo} alt="" className="photo-sketch" />
+        <ul className="side-menu-items">
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {/*{item.icon}*/}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
   );
 }
---
+++ src/components/SidebarData.js
@@ -0,0 +1,38 @@
+import React from "react";
+import * as RiIcons from "react-icons/ri";
+import * as MdIcons from "react-icons/md";
+import * as HiIcons from "react-icons/hi";
+import * as TiIcons from "react-icons/ti";
+
+export const SidebarData = [
+  {
+    title: "ABOUT ME",
+    path: "/",
+    icon: <MdIcons.MdPersonOutline className="sidebar-icon-style" />,
+    cName: "side-text",
+  },
+  {
+    title: "PORTFOLIO",
+    path: "/portfolio",
+    icon: <HiIcons.HiOutlineDesktopComputer className="sidebar-icon-style" />,
+    cName: "side-text",
+  },
+  {
+    title: "MY CV",
+    path: "/mycv",
+    icon: <RiIcons.RiFilePaper2Line className="sidebar-icon-style" />,
+    cName: "side-text",
+  },
+  {
+    title: "BLOG",
+    path: "/blog",
+    icon: <HiIcons.HiOutlineNewspaper className="sidebar-icon-style" />,
+    cName: "side-text",
+  },
+  {
+    title: "CONTACT ME",
+    path: "/contactme",
+    icon: <TiIcons.TiMessages className="sidebar-icon-style" />,
+    cName: "side-text",
+  },
+];
