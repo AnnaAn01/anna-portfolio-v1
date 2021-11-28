98b0883b581ac6377d46e356c566240f0f71e9c9
Anna An <arzumanyan.an@gmail.com>
2021-11-28 22:08:29 +0400

    Added responsive changes to a new branch

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,71 +14,93 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <nav className="side-menu">
-        <Link to="/" className="photo-sketch">
-          <img src={Photo} alt="Profile" className="photo-sketch" />
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className="side-menu-items">
-          <div className="name-link">
-            <Link to="/" className="name-link">
-              <span className="my-name">ANNA DEVELOPER</span>
-              <span className="front-end">FRONT-END DEVELOPER</span>
-            </Link>
-          </div>
-          <div className="menu-item-links">
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-            <div className="get-in-touch">GET IN TOUCH</div>
-            <div className="sidebar-social-media-icons">
-              <span>
-                <a target="_blank" rel="noreferrer" href="https://gmail.com">
-                  <MdIcons.MdEmail className="sidebar-sns" />
-                </a>
-              </span>
+        <div className="name-and-photo">
+          <Link to="/" className="photo-sketch">
+            <img src={Photo} alt="Profile" className="photo-sketch" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}></div>
+          <ul className="side-menu-items">
+            <div className="name-link">
+              <Link to="/" className="name-link">
+                <span className="my-name">ANNA SOFTWAREDEV</span>
+                <span className="front-end">FRONT-END DEVELOPER</span>
+              </Link>
             </div>
-            <span>
-              <a target="_blank" rel="noreferrer" href="https://linkedin.com">
-                <FaIcons.FaLinkedin className="sidebar-sns" />
-              </a>
-            </span>
-            <span>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/AnnaAn01"
-              >
-                <FaIcons.FaGithubSquare className="sidebar-sns" />
-              </a>
-            </span>
-            <span>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://twitter.com/AnnaSoftwareDev"
-              >
-                <FaIcons.FaTwitterSquare className="sidebar-sns" />
-              </a>
-            </span>
-            <span>
-              <a target="_blank" rel="noreferrer" href="https://youtube.com">
-                <FaIcons.FaYoutube className="sidebar-sns" />
-              </a>
-            </span>
-          </div>
-        </ul>
+          </ul>
+        </div>
       </nav>
     </div>
   );
 }
 
 export default Sidebar;
+
+// return (
+//   <div className="sidebar">
+//     <nav className="side-menu">
+//       <Link to="/" className="photo-sketch">
+//         <img src={Photo} alt="Profile" className="photo-sketch" />
+//       </Link>
+//       <div className="menu-icon" onClick={handleClick}>
+//         <i className={click ? "fas fa-times" : "fas fa-bars"} />
+//       </div>
+//       <ul className="side-menu-items">
+//         <div className="name-link">
+//           <Link to="/" className="name-link">
+//             <span className="my-name">ANNA DEVELOPER</span>
+//             <span className="front-end">FRONT-END DEVELOPER</span>
+//           </Link>
+//         </div>
+//         <div className="menu-item-links">
+//           {SidebarData.map((item, index) => {
+//             return (
+//               <li key={index} className={item.cName}>
+//                 <Link to={item.path}>
+//                   {item.icon}
+//                   <span>{item.title}</span>
+//                 </Link>
+//               </li>
+//             );
+//           })}
+//           <div className="get-in-touch">GET IN TOUCH</div>
+//           <div className="sidebar-social-media-icons">
+//             <span>
+//               <a target="_blank" rel="noreferrer" href="https://gmail.com">
+//                 <MdIcons.MdEmail className="sidebar-sns" />
+//               </a>
+//             </span>
+//           </div>
+//           <span>
+//             <a target="_blank" rel="noreferrer" href="https://linkedin.com">
+//               <FaIcons.FaLinkedin className="sidebar-sns" />
+//             </a>
+//           </span>
+//           <span>
+//             <a
+//               target="_blank"
+//               rel="noreferrer"
+//               href="https://github.com/AnnaAn01"
+//             >
+//               <FaIcons.FaGithubSquare className="sidebar-sns" />
+//             </a>
+//           </span>
+//           <span>
+//             <a
+//               target="_blank"
+//               rel="noreferrer"
+//               href="https://twitter.com/AnnaSoftwareDev"
+//             >
+//               <FaIcons.FaTwitterSquare className="sidebar-sns" />
+//             </a>
+//           </span>
+//           <span>
+//             <a target="_blank" rel="noreferrer" href="https://youtube.com">
+//               <FaIcons.FaYoutube className="sidebar-sns" />
+//             </a>
+//           </span>
+//         </div>
+//       </ul>
+//     </nav>
+//   </div>
+// );
+// }
