3b8e25e2da2ab8130fbfe04b0240a78a40049aca
Anna An <arzumanyan.an@gmail.com>
2021-12-13 15:44:36 +0400

    editing the about me page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./AboutMe.css";
 
 function AboutMe() {
-  return <div className="about-me">About me section</div>;
+  return (
+    <div className="about-me">
+      <h3 className="about-title">About Me</h3>
+      <p className="about-text">
+        Hi, i'm Anna, an aspiring front-end web developer.
+      </p>
+      <p className="about-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam
+        nisi quas ipsa magnam. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Fugit totam nisi quas ipsa magnam. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Fugit totam nisi quas ipsa
+        magnam.
+      </p>
+      <h2 className="skills-title">Tech Skills</h2>
+    </div>
+  );
 }
 
 export default AboutMe;
