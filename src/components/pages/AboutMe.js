da1464c7609520cbe82b7f0915c51ec93432fe13
Anna An <arzumanyan.an@gmail.com>
2022-02-16 22:45:45 +0400

    Styling the Contact Me mobile version

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -8,14 +8,31 @@ function AboutMe() {
       <p className="about-text">
         Hi, i'm Anna, an aspiring front-end web developer.
       </p>
-      <p className="about-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam
-        nisi quas ipsa magnam. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Fugit totam nisi quas ipsa magnam. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Fugit totam nisi quas ipsa
-        magnam.
-      </p>
-      <h2 className="skills-title">Tech Skills</h2>
+      <p className="about-text">I speak Armenian, English, Korean, Russian.</p>
+      {/*Skills section */}
+      <div className="skills-container">
+        <h1 className="skills-title">Skills</h1>
+        <div className="skills-wrapper">
+          <div className="skills-card">
+            <h2 className="skills-name">Languages</h2>
+            <p className="skills-list">HTML</p>
+            <p className="skills-list">CSS</p>
+            <p className="skills-list">Javascript</p>
+          </div>
+
+          <div className="skills-card">
+            <h2 className="skills-name">Technologies</h2>
+            <p className="skills-list">React</p>
+            <p className="skills-list">Sass</p>
+            <p className="skills-list">Styled Components</p>
+          </div>
+
+          <div className="skills-card">
+            <h2 className="skills-name">Tools</h2>
+            <p className="skills-list">GitHub</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
