c77ea38f9bf0f31ff54cb0d650e008fa8bff1e5a
Anna An <arzumanyan.an@gmail.com>
2022-02-20 21:37:58 +0400

    recommitting some changes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./AboutMe.css";
+// import BackgroundVideo from "../video/backgroundVideo.mp4";
+// import BackgroundVideo2 from "../video/video2.mp4";
+// import BackgroundVideo3 from "../video/video3.mp4";
 
 function AboutMe() {
   return (
     <div className="about-me">
-      <h3 className="about-title">About Me</h3>
-      <p className="about-text">
-        Hi, i'm Anna, an aspiring front-end web developer.
+      {/*<video className="video-background" autoPlay muted loop>
+        <source src={BackgroundVideo} type="video/mp4" />
+  </video>*/}
+
+      <h3 className="about-title">
+        Hi, I'm Anna,
+        <br /> an aspiring front-end developer.
+      </h3>
+
+      <p className="skills-title">
+        <span className="brackets-span">{"< "}</span>
+        Here's my current tech stack
+        <span className="brackets-span">{" />"}</span>
       </p>
-      <p className="about-text">I speak Armenian, English, Korean, Russian.</p>
+      <span className="aboutme__horizontal-line"></span>
+      {/* <p className="about-text">I speak Armenian, English, Korean, Russian.</p>*/}
       {/*Skills section */}
       <div className="skills-container">
-        <h1 className="skills-title">Skills</h1>
+        {/*<h1 className="skills-title">Skills</h1>*/}
         <div className="skills-wrapper">
           <div className="skills-card">
             <h2 className="skills-name">Languages</h2>
@@ -30,6 +44,7 @@ function AboutMe() {
           <div className="skills-card">
             <h2 className="skills-name">Tools</h2>
             <p className="skills-list">GitHub</p>
+            <p className="skills-list">VSCode</p>
           </div>
         </div>
       </div>
