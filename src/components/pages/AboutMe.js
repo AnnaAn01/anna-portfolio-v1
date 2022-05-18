2cfce808560a6f8bd2da1b2b79ab38b598102765
Anna An <arzumanyan.an@gmail.com>
2022-05-18 20:30:23 +0400

    styling skill cards

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -109,9 +109,9 @@
 
 // export default AboutMe;
 
-import React from "react";
+import React, { useEffect } from "react";
 import "./AboutMe.css";
-
+import Typewriter from "typewriter-effect";
 import {
   SiJavascript,
   SiCss3,
@@ -128,25 +128,47 @@ import { RiReactjsLine } from "react-icons/ri";
 import BackgroundImage from "../../components/image/bg-mountain4.jpg";
 
 function AboutMe() {
+  // const [state] = useEffect({
+  //   title: "Welcome",
+  //   // title2: "I'm Anna Arzumanyan",
+  //   // title3: "I am a front-end developer.",
+  // });
+
   return (
     <div className="about-me">
       <img className="bg-image" src={BackgroundImage} alt="bg" />
       <div className="about-wrapper">
         <div className="about-card">
-          <h3 className="about-title">Hi, I'm Anna, a front-end developer.</h3>
+          {/*<h3 className="about-title">Hi, I'm Anna, a front-end developer.</h3>*/}
+          <h3>
+            <div className="about-title">Welcome</div>
+            {/* <div className="title2">{state.title2}</div>
+            <div className="title3">{state.title3}</div>*/}
+          </h3>
+          <div className="about-title">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+                delay: 80,
+                strings: [
+                  "I'm Anna Arzumanyan.",
+                  "I'm a front-end developer.",
+                  "< Here's my current tech stack / >",
+                ],
+              }}
+            ></Typewriter>
+          </div>
         </div>
         <div className="about-card">
-          <p className="skills-title">
-            {/*<span className="brackets-span">{"<  "}&#160; </span>*/}
+          {/*<p className="skills-title">
             &#60; Here's my current tech stack / &#62;
-            {/*<span className="brackets-span">&#160;{" />"}</span>*/}
-          </p>
+          </p>*/}
         </div>
         <div className="skills-container">
           {/*<h1 className="skills-title">Skills</h1>*/}
           <div className="skills-wrapper">
             <div className="skills-card">
-              <h2 className="skills-name">Languages</h2>
               <p className="skills-list">
                 <span className="tech-icons">
                   <SiHtml5 />
@@ -165,15 +187,20 @@ function AboutMe() {
                 </span>
                 Javascript
               </p>
-            </div>
-
-            <div className="skills-card">
-              <h2 className="skills-name">Technologies</h2>
               <p className="skills-list">
                 <span className="tech-icons">
                   <SiReact />
                 </span>
                 ReactJS
+              </p>
+            </div>
+
+            <div className="skills-card">
+              <p className="skills-list">
+                <span className="tech-icons">
+                  <SiGithub />
+                </span>
+                GitHub
               </p>
               <p className="skills-list">
                 <span className="tech-icons">
@@ -186,22 +213,6 @@ function AboutMe() {
                   <SiStyledcomponents />
                 </span>
                 Styled Components
-              </p>
-            </div>
-
-            <div className="skills-card">
-              <h2 className="skills-name">Tools</h2>
-              <p className="skills-list">
-                <span className="tech-icons">
-                  <SiGithub />
-                </span>
-                GitHub
-              </p>
-              <p className="skills-list">
-                <span className="tech-icons">
-                  <SiVisualstudiocode />
-                </span>
-                VSCode
               </p>
             </div>
           </div>
